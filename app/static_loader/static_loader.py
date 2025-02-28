b418f5275ca6d67c4cb67110cd369349edf903f0
Serhii <mashchenko.serhii@lll.kpi.ua>
2025-02-28 16:01:57 +0200

    Update static_loader.py

--- app/static_loader/static_loader.py
+++ app/static_loader/static_loader.py
@@ -1,7 +1,13 @@
 from pathlib import Path
+from app.static_loader.count_directory import count_directory
 
 def get_static_files (filename):
-    file_path = Path(__file__).parent.parent / "static" / filename
+
+    steps = count_directory("app")
+
+    steps = int(steps) - 1
+
+    file_path = Path(__file__).parents[steps] / "static" / filename
 
     with open(file_path, "r") as file:
         return file.read()
